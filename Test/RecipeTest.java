7c805ca6d0d1a4169b2bd19df16126f7f8cd21d6
david rydman <david.rydman@iths.se>
2024-12-09 23:18:01 +0100

    added RecipeTest (in progress) Creating Recipe class

--- Test/RecipeTest.java
+++ Test/RecipeTest.java
@@ -1,2 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RecipeTest {
+
+    @Test
+    void testRecipeCreation() {
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient("Ham", "700g"));
+        Recipe recipe = new Recipe("Christmas Ham", ingredients, "Slice and cook.", new Category("Breakfast"));
+
+        assertEquals("Christmas Ham", recipe.getTitle());
+        assertEquals(1, recipe.getIngredients().size());
+        assertEquals("Slice and cook.", recipe.getInstructions());
+        assertEquals("Breakfast", recipe.getCategory().getName());
+    }
 }
