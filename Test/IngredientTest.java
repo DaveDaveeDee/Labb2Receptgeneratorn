b31b4919b2a67427c611d7373e70c16552aacdc0
david rydman <david.rydman@iths.se>
2024-12-09 22:48:27 +0100

    added IngredientTest, Ingredient class created (DONE) Now adding ingredientTest for toString (in progress)

--- Test/IngredientTest.java
+++ Test/IngredientTest.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IngredientTest {
 
     @Test
     void testIngredientCreation() {
-        Ingredient ingredient = new Ingredient("Honey, 1spoon");
+        Ingredient ingredient = new Ingredient("Honey", "1spoon");
         assertEquals("Honey", ingredient.getName());
         assertEquals("1spoon", ingredient.getQuantity());
     }
+
+
 }
--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,4 +1,18 @@
 public class Ingredient {
-    public Ingredient(String s) {
+    private String name;
+    private String quantity;
+
+    public Ingredient(String name, String quantity) {
+        this.name = name;
+        this.quantity = quantity;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
 }
