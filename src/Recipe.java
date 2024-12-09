a906772a50e6304b5c46f6df4a2f0e120381b055
david rydman <david.rydman@iths.se>
2024-12-09 23:25:27 +0100

    added RecipeTest (in progress) Creating Recipe class (in progress) Creating toString (in progress)

--- src/Recipe.java
+++ src/Recipe.java
@@ -7,6 +7,25 @@ public class Recipe {
     private Category category;
 
     public Recipe(String title, List<Ingredient> ingredients, String instructions, Category category) {
+        this.title = title;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.category = category;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public Category getCategory() {
+        return category;
     }
 }
