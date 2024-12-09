f0beb61697562157da13e8ace5142c910c5da8d8
david rydman <david.rydman@iths.se>
2024-12-09 22:57:33 +0100

    added CategoryTest (DONE) Creating Category.class (in progress)

--- Test/CategoryTest.java
+++ Test/CategoryTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CategoryTest {
+
+    @Test
+    void testCategoryCreation() {
+        Category category = new Category("Breakfast");
+        assertEquals("Breakfast", category.getName());
+    }
 }
