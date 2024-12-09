88129e0c945442356b27d5daa210be160fdd4c44
david rydman <david.rydman@iths.se>
2024-12-09 22:11:32 +0100

    Test greeting in progress

---
+++ Test/GreetingTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GreetingTest {
+
+    @Test
+    void Greeting() {
+        Greeting greeting = new Greeting();
+        String result = greeting.greet();
+        System.out.println("Result from the greet method is: " + result); // TODO ta bort efter att den fungerar
+        assertEquals("\n\t Welcome \n \t\tto\n witches cauldron", result, "The greet method should return 'Welcome to witches cauldron'");
+    }
+}
