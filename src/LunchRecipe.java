ba919cd80ce1ab6c14478fe72b298774eeefa3fc
david rydman <david.rydman@iths.se>
2024-12-10 08:44:30 +0100

    Lunch class DONE

--- src/LunchRecipe.java
+++ src/LunchRecipe.java
@@ -1,3 +1,8 @@
-public class LunchRecipe {
+import java.util.List;
 
+public class LunchRecipe extends Recipe {
+
+    public LunchRecipe(String title, List<Ingredient> ingredients, String instructions) {
+        super(title, ingredients, instructions, new Category("Lunch"));
+    }
 }
